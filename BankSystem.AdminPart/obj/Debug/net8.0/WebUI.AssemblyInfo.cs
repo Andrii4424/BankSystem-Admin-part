81e3d33ca770d18acacc9cb1465542e73c349902
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-06-27 23:04:17 +0300

    Updating layout view

--- BankSystem.AdminPart/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ BankSystem.AdminPart/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d5188895dd30e7c1e5b59635fa4d3a04c036a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71cb6bbcce0e5d45970cc0c9fbab9ba5f5fb4d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
