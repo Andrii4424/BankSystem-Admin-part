a38cb40090bdb9458022ee940818b7dd0c477b2d
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-06-29 15:04:17 +0300

    Added menu expansion

--- WebUI/wwwroot/js/_Layout.js
+++ WebUI/wwwroot/js/_Layout.js
@@ -1,12 +1,21 @@
 const openMenuIcon = document.getElementById("open-menu");
 const openMenuBlock = document.getElementById("open-menu-block");
 const closeMenuIcon = document.getElementById("close-menu");
+const menuBackground = document.getElementById("open-menu-background");
+const openMenuIcons = document.getElementById("open-menu-icons");
+const iconBlock = document.getElementById("icon-block");
 
-openMenuBlock.addEventListener("click", OpenMenu);
+
+
+
+iconBlock.addEventListener("click", OpenMenu);
+
 
 function OpenMenu() {
     openMenuIcon.classList.toggle("clicked");
     closeMenuIcon.classList.toggle("clicked");
+    menuBackground.classList.toggle("opened");
+    openMenuIcons.classList.toggle("opened");
     /*openMenuIcon.style.display = "none";
     openMenuBlock.style.display = "none";
 
