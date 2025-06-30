a896f59d5468373a8454b4a8763304ab1c996ccd
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-06-30 10:42:49 +0300

    Adding links to sidebar

--- WebUI/wwwroot/js/_Layout.js
+++ WebUI/wwwroot/js/_Layout.js
@@ -2,8 +2,9 @@ const openMenuIcon = document.getElementById("open-menu");
 const openMenuBlock = document.getElementById("open-menu-block");
 const closeMenuIcon = document.getElementById("close-menu");
 const menuBackground = document.getElementById("open-menu-background");
-const openMenuIcons = document.getElementById("open-menu-icons");
+const openMenuIcons = document.getElementById("open-menu-page");
 const iconBlock = document.getElementById("icon-block");
+const menuContent = document.getElementById("menu-content");
 
 
 
@@ -16,6 +17,7 @@ function OpenMenu() {
     closeMenuIcon.classList.toggle("clicked");
     menuBackground.classList.toggle("opened");
     openMenuIcons.classList.toggle("opened");
+    menuContent.classList.toggle("opened");
     /*openMenuIcon.style.display = "none";
     openMenuBlock.style.display = "none";
 
