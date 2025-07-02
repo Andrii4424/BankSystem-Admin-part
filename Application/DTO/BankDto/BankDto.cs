58a8ebe4bbf4e716c5a6e8f6eb8f90889924af06
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-07-02 11:53:56 +0300

    Adding bank and card tariffs dto

---
+++ Application/DTO/BankDto/BankDto.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.BankDto
+{
+    public class BankDto
+    {
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Id")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [StringLength(40)]
+        [Display(Name = "Bank name")]
+        public string BankName { get; set; }
+
+        [Range(1.0, 5.0, ErrorMessage = "{0} must be between {1} and {2}")]
+        [Display(Name = "Bank rating")]
+        public double Rating { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Has License")]
+        public bool HasLicense { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [StringLength(40)]
+        [Display(Name = "Bank founder full name")]
+        public string BankFounderFullName { get; set; }
+
+        //Internal bank information
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [StringLength(40)]
+        [Display(Name = "Bank director full name")]
+        public string BankDirectorFullName { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "{0} cant be lesser than {1} or empty")]
+        [Display(Name = "Capitalization")]
+        public double Capitalization { get; set; }
+
+        [BindNever]
+        public int EmployeesCount { get; set; }
+
+        [BindNever]
+        public int BlockedClientsCount { get; set; }
+
+        [BindNever]
+        public int ClientsCount { get; set; }
+
+        [BindNever]
+        public int ActiveClientsCount { get; set; }
+
+        [StringLength(100)]
+        public string? WebsiteUrl { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [StringLength(20)]
+        [Display(Name = "Contact phone")]
+        public string ContactPhone { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Established date")]
+        public DateTime EstablishedDate { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [StringLength(100)]
+        [Display(Name = "Legal address")]
+        public string LegalAddress { get; set; }
+    }
+}
---
+++ Application/DTO/BankDto/CardTariffsDto.cs
@@ -0,0 +1,82 @@
+﻿using Domain.Entities.Banks;
+using Domain.Enums.CardEnums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.BankDto
+{
+    public class CardTariffsDto :IValidatableObject
+    {
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Id")]
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Id")]
+        public Guid BankId { get; private set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [StringLength(40)]
+        [Display(Name = "Card name")]
+        public string CardName { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Card type")]
+        public CardType Type { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Card level")]
+        public CardLevel Level { get; set; }
+
+        [Range(1, 99, ErrorMessage = "{0} must be between {1} and {2}")]
+        [Display(Name = "Validity period")]
+        public double ValidityPeriod { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "{0} cant be lesser than 0 or empty")]
+        [Display(Name = "Max credit limit")]
+        public int MaxCreditLimit { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Payment system")]
+        public PaymentSystem[] EnabledPaymentSystem { get; set; }
+
+        public double? InterestRate { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Curency")]
+        public string[] Curency { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [Display(Name = "Annual maintenance cost")]
+        public int AnnualMaintenanceCost { get; set; }
+
+        [Range(0, 25.00, ErrorMessage = "{0} must be between {1} and {2}")]
+        [Display(Name = "Max credit limit")]
+        public double P2PToAnotherBankCommission { get; set; }
+
+        [Range(0, 10.00, ErrorMessage = "{0} must be between {1} and {2}")]
+        [Display(Name = "Max credit limit")]
+        public double P2PInternalCommission { get; set; }
+
+        [Required(ErrorMessage = "{0} has to be provided")]
+        [StringLength(4)]
+        [Display(Name = "Card number masked")]
+        public string CardNumberMasked { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (P2PInternalCommission > P2PToAnotherBankCommission) yield return new ValidationResult("P2P commision to " +
+                "another bank cant be lesser than internal P2P commision"); 
+            if(Type == CardType.Debit && MaxCreditLimit>0) yield return new ValidationResult("Debit card cant have credit limit");
+            if(Type == CardType.Debit && InterestRate!=null) yield return new ValidationResult("Debit card cant have interest rate");
+            if (ValidityPeriod % 0.5 != 0) yield return new ValidationResult("The card validity period must be a multiple of 1 year " +
+                "or half a year (0.5)");
+            throw new NotImplementedException();
+        }
+    }
+}
