13dec5015abd235b53fcc2a8c7a2f07e08f19687
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-07-03 09:04:19 +0300

    Add BankController and ModelBindingFilter

---
+++ WebUI/Controllers/BankController.cs
@@ -0,0 +1,28 @@
+﻿using Application.ServiceContracts.BankServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class BankController : Controller
+    {
+        private readonly IBankReadService _bankReadService;
+        private readonly IBankAddService _bankAddService;
+        private readonly IBankUpdateService _bankUpdateService;
+        private readonly IBankDeleteService _bankDeleteService;
+
+        public BankController(IBankReadService bankReadService, IBankAddService bankAddService, IBankUpdateService bankUpdateService,
+            IBankDeleteService bankDeleteService)
+        {
+            _bankReadService = bankReadService;
+            _bankAddService = bankAddService;
+            _bankUpdateService = bankUpdateService;
+            _bankDeleteService = bankDeleteService;
+        }
+
+        [Route("/bank")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
