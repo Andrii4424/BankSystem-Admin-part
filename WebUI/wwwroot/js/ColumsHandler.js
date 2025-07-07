b96daa9284a68dfc1b827f5ec81ca4c854c41456
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-07-07 23:51:26 +0300

    Add column icons, rename image folders

---
+++ WebUI/wwwroot/js/ColumsHandler.js
@@ -0,0 +1,12 @@
+﻿const oneColumn = document.getElementById("one-column");
+const twoColumns = document.getElementById("two-colums");
+
+oneColumn.addEventListener("click", () => {
+    oneColumn.classList.add("chosen");
+    twoColumns.classList.remove("chosen");
+});
+
+twoColumns.addEventListener("click", () => {
+    twoColumns.classList.add("chosen");
+    oneColumn.classList.remove("chosen");
+});
