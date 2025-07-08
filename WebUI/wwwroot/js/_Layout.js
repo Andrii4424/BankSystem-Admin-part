95fe0ab62b72706bbfc778c95019a9939ef7cb05
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-07-08 12:22:30 +0300

    update sidebar in _Layout.cshtml

--- WebUI/wwwroot/js/_Layout.js
+++ WebUI/wwwroot/js/_Layout.js
@@ -1,16 +1,16 @@
-const menu = document.getElementById("menu");
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
+const menu = document.getElementById("menu");
+
 
 openMenu.addEventListener("click", () => {
-    menu.classList.add("opened");
     openMenu.classList.add("clicked");
     closeMenu.classList.remove("clicked");
+    menu.classList.add("opened");
 });
 
-
 closeMenu.addEventListener("click", () => {
-    menu.classList.remove("opened");
     openMenu.classList.remove("clicked");
     closeMenu.classList.add("clicked");
+    menu.classList.remove("opened");
 });
