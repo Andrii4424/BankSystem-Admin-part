af2844e4458c50b99d7361f012e354b95250efd3
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-07-26 10:47:26 +0300

    Update CardTariffsController and add views

---
+++ Application/DTO/FiltersDto/BankFilters.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Entities.Banks;
+using System.Linq.Expressions;
+
+namespace Application.DTO.FiltersDto
+{
+    public class BankFilters
+    {
+        public int FirstElement { get; set; }
+        public int ElementsToLoad { get; set; }
+
+        //Filters, search and sort settings
+        public string? SearchValue { get; set; }
+
+        public string SortValue { get; set; }
+        public bool ascending {  get; set; }
+
+        public bool? licenseFilter { get; set; } 
+        public bool? siteFilter { get; set; } 
+        public double? ratingFilter { get; set; }
+        public int? clientsCountFilter { get; set; }
+        public int? capitalizationFilter { get; set; }
+
+        public BankFilters()
+        {
+            FirstElement = 0;
+            ElementsToLoad = 6;
+        }
+
+        public Filters<BankEntity> ToGeneralFilters()
+        {
+            Expression<Func<BankEntity, bool>>? searchFilter = SearchValue != null ? c => c.BankName.Contains(SearchValue.Trim()) : null;
+            Expression<Func<BankEntity, object>>? sortExpression;
+
+            ascending = true;
+            switch (SortValue)
+            {
+                case "oldest":
+                    sortExpression = b => b.EstablishedDate;
+                    break;
+                case "newest":
+                    sortExpression = b => b.EstablishedDate;
+                    ascending = false;
+                    break;
+                case "rating-descending":
+                    sortExpression = b => b.Rating;
+                    ascending = false;
+                    break;
+                case "popularity-descending":
+                    sortExpression = b => b.ActiveClientsCount;
+                    ascending = false;
+                    break;
+                default:
+                    sortExpression = b => b.EstablishedDate;
+                    break;
+            }
+
+            List<Expression<Func<BankEntity, bool>>?> filters = new();
+
+            filters.Add((licenseFilter.HasValue && licenseFilter == true) ? b => b.HasLicense : null);
+            filters.Add((siteFilter.HasValue && siteFilter == true) ? b => b.WebsiteUrl != null : null);
+            filters.Add((ratingFilter.HasValue && ratingFilter != 0) ? b => b.Rating >= ratingFilter : null);
+            filters.Add((clientsCountFilter.HasValue && clientsCountFilter != 0) ? b => b.ActiveClientsCount >= clientsCountFilter : null);
+            filters.Add((capitalizationFilter.HasValue && capitalizationFilter != 0) ? b => b.Capitalization >= capitalizationFilter : null);
+
+            return new Filters<BankEntity>(FirstElement, ElementsToLoad, searchFilter, sortExpression, ascending, filters);
+        }
+
+    }
+}
--- WebUI/Controllers/CardTariffsController.cs
+++ WebUI/Controllers/CardTariffsController.cs
@@ -1,6 +1,9 @@
-﻿using Application.ServiceContracts.ICardTarrifsService;
-using Microsoft.AspNetCore.Mvc;
+﻿using Application.DTO.BankProductDto;
 using Application.DTO.FiltersDto;
+using Application.ServiceContracts.BankServiceContracts;
+using Application.ServiceContracts.ICardTarrifsService;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Filters;
 
 namespace WebUI.Controllers
 {
@@ -10,21 +13,43 @@ namespace WebUI.Controllers
         private readonly ICardTarrifsAddService _cardTarrifsAddService;
         private readonly ICardTarrifsUpdateService _cardTarrifsUpdateService;
         private readonly ICardTarrifsDeleteService _cardTarrifsDeleteService;
+        private readonly IBankReadService _bankReadService;
 
         public CardTariffsController(ICardTarrifsReadService cardTarrifsReadService, ICardTarrifsAddService cardTarrifsAddService,
-            ICardTarrifsUpdateService cardTarrifsUpdateService, ICardTarrifsDeleteService cardTarrifsDeleteService)
+            ICardTarrifsUpdateService cardTarrifsUpdateService, ICardTarrifsDeleteService cardTarrifsDeleteService, IBankReadService bankReadService)
         {
             _cardTarrifsReadService = cardTarrifsReadService;
             _cardTarrifsAddService = cardTarrifsAddService;
             _cardTarrifsUpdateService = cardTarrifsUpdateService;
             _cardTarrifsDeleteService = cardTarrifsDeleteService;
+            _bankReadService = bankReadService;
         }
-
 
         [Route("/cards")]
         public async Task<IActionResult> CardTariffsList()
         {
             return View(await _cardTarrifsReadService.GetCardsAsync(new CardTariffsFilters()));
+        }
+        //Add Actions
+        [Route("/add-card")]
+        public async Task<IActionResult> ChoseBankAddCard()
+        {
+            return View("ChoseBank" ,await _bankReadService.GetLimitedAsyncByDtoFilter(new BankFilters()));
+        }
+
+        [HttpGet("/add-card/bank-id/{bankId:guid}")]
+        public async Task<IActionResult> AddCard([FromRoute] Guid bankId)
+        {
+            return View(new CardTariffsDto());
+        }
+
+        [TypeFilter(typeof(ModelBindingFilter))]
+        [HttpPost("/add-card/bank-id/{bankId:guid}")]
+        public async Task<IActionResult> AddCard([FromRoute] Guid bankId, [FromForm] CardTariffsDto cardDto)
+        {
+            cardDto.BankId = bankId;
+            await _cardTarrifsAddService.AddCardAsync(cardDto);
+            return View();
         }
     }
 }
