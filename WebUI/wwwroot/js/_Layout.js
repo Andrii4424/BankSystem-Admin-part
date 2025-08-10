71ed13f514cb0805c4c484ec7e02f46228c12f27
Andrii4424 <andreyfalchevskyi@gmail.com>
2025-08-10 10:58:48 +0300

    Add language panel styles

--- WebUI/wwwroot/js/_Layout.js
+++ WebUI/wwwroot/js/_Layout.js
@@ -1,6 +1,8 @@
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
 const menu = document.getElementById("menu");
+const languageName = document.getElementById("language-name");
+const choseLanguageMenu = document.getElementById("chose-language");
 
 
 openMenu.addEventListener("click", () => {
@@ -13,4 +15,15 @@ closeMenu.addEventListener("click", () => {
     openMenu.classList.remove("clicked");
     closeMenu.classList.add("clicked");
     menu.classList.remove("opened");
+});
+
+//Language
+languageName.addEventListener("click", () => {
+    choseLanguageMenu.classList.toggle("opened");
+});
+
+document.body.addEventListener("click", (event) => {
+    if (!event.target.closest("#language")) {
+        choseLanguageMenu.classList.remove("opened");
+    }
 });
